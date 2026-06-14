dfcb4133bc10e5c31e1f89bda9609f17b99f4fe4
Jerry Lopez <jerry@92labs.com>
2026-06-13 21:37:20 -0500

    Use R2 bucket for remote storage

--- app/etc/env.php
+++ app/etc/env.php
@@ -149,7 +149,18 @@ return [
         'frontName' => $_ENV['BACKEND_NAME']
     ],
     'remote_storage' => [
-        'driver' => 'file'
+        'driver' => 'aws-s3',
+        'config' => [
+            'bucket' => $_ENV['AWS_BUCKET'],
+            'region' => $_ENV['AWS_DEFAULT_REGION'],
+            'endpoint' => $_ENV['AWS_ENDPOINT'],
+            'use_path_style_endpoint' => false,
+            'credentials' => [
+                'key' => $_ENV['AWS_ACCESS_KEY_ID'],
+                'secret' => $_ENV['AWS_SECRET_ACCESS_KEY'],
+            ],
+        ],
+        'prefix' => '',
     ],
     'config' => [
         'async' => 0
